539e82ef1c925515565bf320a7ea8af74ff44aa8
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2020-03-04 18:44:55 +0300

    Homepage Responsive Working

--- assets/src/script/plugin/app.js
+++ assets/src/script/plugin/app.js
@@ -66,6 +66,17 @@ jQuery(function($) {
             var headerHeight = $("header.header");
             $("header + section").css("min-height", headerHeight.height() + "px").css("padding-top", headerHeight.height() + "px");
         });
+    } else {
+        $(window).on("load resize scroll",function(e){
+            var scroll = $(window).scrollTop();
+            var headerHeight = $("header.header");
+
+            if (scroll > 10) {
+                headerHeight.addClass("navscroll");
+            } else {
+                headerHeight.removeClass("navscroll");
+            }
+        })
     }
 });
 /* Header Web Navbar Fixed */
